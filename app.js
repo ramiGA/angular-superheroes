2dc7837f77cb890398997ee717f315e78cd0fcb0
ramiGA <ramisn3@gmail.com>
2017-08-13 18:07:34 -0400

    add scope prop+mvc

---
+++ app.js
@@ -0,0 +1,1 @@
+angular.module('super', ['ui.bootstrap']);
---
+++ controllers/supersController.js
@@ -0,0 +1,13 @@
+angular
+        .module('super')
+        .controller('supersController', function($scope){
+
+  $scope.Heros = [{
+    name: "Spider Man", phone: "6789", comics: "Marvel"},
+  {name: "Bat Man", phone: "1234556", comics: "DC"},
+{name: "Iron Man", phone: "888888", comics: "Marvel"},
+{name: "Super Man", phone: "093957", comics: "DC"},
+{name: "DeadPool", phone: "898098", comics: "Marvel"},
+{name: "Wolverine", phone: "00000", comics: "Marvel"}
+]
+});
