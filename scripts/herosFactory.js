45bffc0cf310772c52f6aa5d66f9b32fe56a1d8f
ramiGA <ramisn3@gmail.com>
2017-08-13 22:52:16 -0400

    add data.json+python server

--- scripts/herosFactory.js
+++ scripts/herosFactory.js
@@ -1,17 +1,8 @@
 angular
     .module('super')
-    .factory('herosFactory', function () {
-
-  var superData = [
-{name: "Spider Man", phone: "6789876543", comics: "Marvel"},
-{name: "Bat Man", phone: "1234567890", comics: "DC"},
-{name: "Iron Man", phone: "7654321985", comics: "Marvel"},
-{name: "Super Man", phone: "9876543210", comics: "DC"},
-{name: "DeadPool", phone: "9085674321", comics: "Marvel"},
-{name: "Wolverine", phone: "9098675435", comics: "Marvel"}
-];
+    .factory('herosFactory', function ($http) {
 function getSuperdata() {
-  return superData;
+  return $http.get('data/data.json');
 }
 return{
   getSuperdata: getSuperdata
--- scripts/supersController.js
+++ scripts/supersController.js
@@ -2,5 +2,12 @@ angular
         .module('super', ['ui.bootstrap'])
         .controller('supersController', function($scope, herosFactory){
 
-  $scope.Heros = herosFactory.getSuperdata();
+            $scope.Heros;
+             herosFactory.getSuperdata().success(function (data) {
+               $scope.Heros = data;
+
+             }).error(function (error) {
+               console.log(error);
+
+             });
 });
